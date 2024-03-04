44540a7d5c3bfb0bc89f4c375f037b8f9e5ec0c8
julia.andersson1 <jande182@volvocars.com>
2024-03-04 14:20:11 +0100

    hej

--- myquiz.py
+++ myquiz.py
@@ -1,6 +1,7 @@
 import json
 import random
 import copy
+import pytest
 
 #classes for the json-file
 class Question:
@@ -34,54 +35,54 @@ print()
 user_name = input("Username: ").lower()
 
 
-selected_category is None
+selected_category = None
 
 while not selected_category:
-        user_input = input(
-            "Choose the difficulty of the quiz: (Easy/Medium/Hard) "
-        ).lower()
-        print()
+    user_input = input(
+        "Choose the difficulty of the quiz: (Easy/Medium/Hard) "
+    ).lower()
+    print()
 
-        for category in categories:
-            if category.name.lower() == user_input:
-                selected_category = category
-                break
+    for category in categories:
+        if category.name.lower() == user_input:
+            selected_category = category
+            break
 
-        if selected_category is None:
-            print("Choose either easy, medium or hard!")
+    if selected_category is None:
+        print("Choose either easy, medium or hard!")
 
-    selected_category2 = copy.deepcopy(selected_category)
+selected_category2 = copy.deepcopy(selected_category)
 
-    while selected_category2.questions:
+while selected_category2.questions:
 
-        #game starts
-        random_question = selected_category2.get_random_questions()
+    #game starts
+    random_question = selected_category2.get_random_questions()
 
-        user_input = input(f"{random_question.question} ")
+    user_input = input(f"{random_question.question} ")
 
-        #result
-        if user_input.lower() == random_question.answer.lower():
-            POINTS = POINTS + 1
-            print("Right answer!!! Score: " + str(POINTS))
-        else:
-            print("Wrong answer. The correct answer is :", random_question.answer)
+    #result
+    if user_input.lower() == random_question.answer.lower():
+        POINTS = POINTS + 1
+        print("Right answer!!! Score: " + str(POINTS))
+    else:
+        print("Wrong answer. The correct answer is :", random_question.answer)
 
-        selected_category2.questions.remove(random_question)
-        print()
+    selected_category2.questions.remove(random_question)
+    print()
 
-    print("Round is finished " + user_input_name + "! Score: " + str(POINTS))
+print("Round is finished " + user_name + "! Score: " + str(POINTS))
 
 
-    #keep playing or not
-    CONTINUE_PLAYING = None
+#keep playing or not
+CONTINUE_PLAYING = None
 
-    while CONTINUE_PLAYING not in ('yes', 'no'):
-        CONTINUE_PLAYING = input("Do you want to keep playing? (Yes/No) ").lower()
-        print()
+while CONTINUE_PLAYING not in ('yes', 'no'):
+    CONTINUE_PLAYING = input("Do you want to keep playing? (Yes/No) ").lower()
+    print()
 
-        for CONTINUE_PLAYING in input():
-            if CONTINUE_PLAYING in ('yes', 'no'):
-                break
+    for CONTINUE_PLAYING in input():
+        if CONTINUE_PLAYING in ('yes', 'no'):
+            break
 
-        if CONTINUE_PLAYING not in ('yes', 'no'):
-            print("Choose either yes or no: ")
+    if CONTINUE_PLAYING not in ('yes', 'no'):
+        print("Choose either yes or no: ")

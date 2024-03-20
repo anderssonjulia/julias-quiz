97d8fa14be374fe57c652fd4787002f5e25536dc
julia.andersson1 <jande182@volvocars.com>
2024-03-20 09:20:28 +0100

    update: fix pylint fail

--- myquiz.py
+++ myquiz.py
@@ -22,19 +22,19 @@ class Question:
 
     def display_question(self):
         """Dispay the question."""
-        print(f"Question: {self.question}")
+        return {self.question}
 
     def display_answer(self):
         """Display the answer."""
-        print(f"Answer: {self.answer}")
+        return {self.answer}
 
     def is_correct(self, user_answer):
         """Check if the user's input is the correct answer."""
         return user_answer.lower() == self.answer.lower()
 
 
-class Category:
-    """Class documentation for Category."""
+class Difficulty:
+    """Class documentation for difficulty."""
 
     def __init__(self, name, questions):
         """Constructor documentation."""
@@ -53,32 +53,57 @@ class Category:
         return random.choice(self.questions)
 
     def get_all_questions(self):
-        """Get all questions in category."""
+        """Get all questions in difficulty."""
         return self.questions
 
 
-def get_category_list(json_file_path):
+def game_starts():
+    """docstring"""
+    continue_playing = True
+
+    total_score = 0
+    json_file_path = "myquiz.json"
+    difficulty_list = get_difficulty_list(json_file_path)
+
+    user_input_name = get_user_input_name()
+
+    while continue_playing:
+
+        selected_difficulty = player_choose_difficulty(difficulty_list)
+
+        selected_difficulty_copy = copy.deepcopy(selected_difficulty)
+
+        # Select question, get answer and get point
+        total_score += run_quiz_round(selected_difficulty_copy, total_score)
+
+        print("Round is finished " + user_input_name + "! Score: " + str(total_score))
+
+        # Keep playing or not
+        continue_playing = player_wants_to_continue_playing()
+
+
+def get_difficulty_list(json_file_path):
     """docstring"""
 
-    # Get Categories dict from json file
+    # Get Difficulties dict from json file
     with open(json_file_path, "r", encoding="utf-8") as json_file:
         json_data = json_file.read()
 
     data = json.loads(json_data)
 
-    # Create Catergories objects
-    category_list = []
+    # Create Difficulty objects
+    difficulty_list = []
 
-    for category_data in data["categories"]:
-        category = Category(
-            name=category_data["name"], questions=category_data["questions"]
+    for difficulty_data in data["difficulties"]:
+        difficulty = Difficulty(
+            name=difficulty_data["name"], questions=difficulty_data["questions"]
         )
-        category_list.append(category)
+        difficulty_list.append(difficulty)
 
-    return category_list
+    return difficulty_list
 
 
-def startup():
+def get_user_input_name():
     """docstr"""
     print("QUIZ")
     print()
@@ -87,32 +112,32 @@ def startup():
     return user_input_name
 
 
-def player_choose_category(category_list):
+def player_choose_difficulty(difficulty_list):
     """docstr"""
-    selected_category = None
+    selected_difficulty = None
 
-    while selected_category is None:
+    while selected_difficulty is None:
         user_input_choose_difficulty = input(
             "Choose the difficulty of the quiz: (Easy/Medium/Hard) "
         ).lower()
         print()
 
-        for category in category_list:
-            if category.name.lower() == user_input_choose_difficulty:
-                selected_category = category
+        for difficulty in difficulty_list:
+            if difficulty.name.lower() == user_input_choose_difficulty:
+                selected_difficulty = difficulty
 
-        if selected_category is None:
+        if selected_difficulty is None:
             print("Choose either easy, medium or hard!")
 
-    return selected_category
+    return selected_difficulty
 
 
-def question_and_answer(selected_category_copy, total_score):
+def run_quiz_round(selected_difficulty_copy, total_score):
     """docstr"""
     round_score = 0
 
-    while selected_category_copy.questions:
-        random_question = selected_category_copy.get_random_questions()
+    while selected_difficulty_copy.questions:
+        random_question = selected_difficulty_copy.get_random_questions()
         user_question_answer = input(f"{random_question.question}")
 
         if random_question.is_correct(user_question_answer):
@@ -121,7 +146,7 @@ def question_and_answer(selected_category_copy, total_score):
         else:
             print("Wrong answer. The correct answer is :", random_question.answer)
 
-        selected_category_copy.questions.remove(random_question)
+        selected_difficulty_copy.questions.remove(random_question)
         print()
 
     return round_score
@@ -137,33 +162,8 @@ def player_wants_to_continue_playing():
             return True
         if yes_or_no == "no":
             return False
-        else:
+        if yes_or_no not in ("yes", "no"):
             print("Choose either yes or no: ")
-
-
-def game_starts():
-    """docstring"""
-    continue_playing = True
-
-    total_score = 0
-    json_file_path = "myquiz.json"
-    category_list = get_category_list(json_file_path)
-
-    user_input_name = startup()
-
-    while continue_playing:
-
-        selected_category = player_choose_category(category_list)
-
-        selected_category_copy = copy.deepcopy(selected_category)
-
-        # Select question, get answer and get point
-        total_score += question_and_answer(selected_category_copy, total_score)
-
-        print("Round is finished " + user_input_name + "! Score: " + str(total_score))
-
-        # Keep playing or not
-        continue_playing = player_wants_to_continue_playing()
 
 
 def main():

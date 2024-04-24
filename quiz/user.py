be1cf238316b96cfe853d37e5f3a08607d641eeb
julia.andersson1 <jande182@volvocars.com>
2024-04-24 09:52:18 +0200

    add: pylint disable on user module

--- quiz/user.py
+++ quiz/user.py
@@ -2,12 +2,12 @@
 
 
 class User:
+    # pylint: disable=too-few-public-methods
     """Has username and score attributes that are initially None and 0."""
 
     def __init__(self, username=None, score=0):
         self.username = username
         self._score = score
-
 
     @property
     def score(self):
